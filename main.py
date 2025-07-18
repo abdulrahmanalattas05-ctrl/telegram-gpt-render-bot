62ed3feecae345a067f14b872610232468bb02ef
abdulrahmanalattas05-ctrl <abdulrahman.alattas.05@gmail.com>
2025-07-18 17:11:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from telegram import Update
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CommandHandler
 
 # مفاتيح من بيئة التشغيل
-openai.api_key = os.getenv("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")
-telegram_token = os.getenv("8066239879:AAGURepbswUiGB210v931Zu95mBswhXfVVs")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+telegram_token = os.getenv("TELEGRAM_BOT_TOKEN")
 
 # رسالة الترحيب
 def start(update: Update, context: CallbackContext):
